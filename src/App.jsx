0056ca28c46235645f9e7d238a1d3a92bca892df
EloM28 <mugisheloge@gmail.com>
2025-07-14 16:30:12 +0200

    Added in App the component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import SearchBox from './components/SearchBox'
+import CategoryFilter from './components/CategoryFilter';
+import RecentListings from './components/RecentListings';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='font-bold'> WELCOME</h1>
-    </>
-  )
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <div className="relative">
+        <HeroSection />
+        {/* <div className="absolute left-0 right-0 flex justify-center" style={{top: '320px'}}>
+          <SearchBox />
+        </div> */}
+      </div>
+      <div className="pt-2">
+        <CategoryFilter />
+        <RecentListings />
+      </div>
+    </div>
+  );
 }
 
 export default App
