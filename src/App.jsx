f5d771667de89dd634efb33d2ff87a6434995176
EloM28 <mugisheloge@gmail.com>
2025-07-14 17:29:03 +0200

    Changed the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,22 +8,30 @@ import HeroSection from './components/HeroSection'
 import SearchBox from './components/SearchBox'
 import CategoryFilter from './components/CategoryFilter';
 import RecentListings from './components/RecentListings';
+import ListingPage from './components/ListingPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Navbar />
-      <div className="relative">
-        <HeroSection />
-        {/* <div className="absolute left-0 right-0 flex justify-center" style={{top: '320px'}}>
-          <SearchBox />
-        </div> */}
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={
+            <>
+            <Navbar />
+            <div className="relative">
+              <HeroSection />
+            </div>
+            <div className="pt-2">
+              <CategoryFilter />
+              <RecentListings />
+            </div>
+            </>
+            
+          } />
+          <Route path="/ListingPage" element={<ListingPage />} />
+        </Routes>
       </div>
-      <div className="pt-2">
-        <CategoryFilter />
-        <RecentListings />
-      </div>
-    </div>
+    </Router>
   );
 }
 
