4dbc6e30dfc1ec44a8b206466513695f0fede881
EloM28 <mugisheloge@gmail.com>
2025-07-17 15:12:49 +0200

    Changed the App

--- src/App.jsx
+++ src/App.jsx
@@ -15,38 +15,41 @@ import AuthForm from './components/AuthForm';
 import ForgotPassword from './components/ForgotPassword';
 import Dashboard from './components/Dashboard';
 import ExplorePage from './components/ExplorePage';
+import { AuthProvider } from './components/AuthContext';
 
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-      <Navbar />
-        <Routes>
-          <Route path="/" element={
-            <>
-            <div className="relative">
-              <HeroSection />
-            </div>
-            <div className="pt-2">
-              <CategoryFilter />
-              <RecentListings />
-              <ListingSaleGrid />
-            </div>
-            </>
-            
-          } />
-          <Route path="/ListingPage" element={<ListingPage />} />
-          <Route path="/rentlist" element={<RentList />} />
-          <Route path="/signin" element={<AuthForm />} />
-          <Route path="/register" element={<AuthForm />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/explore" element={<ExplorePage />} />
-        </Routes>
-       <FooterSection />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={
+              <>
+              <div className="relative">
+                <HeroSection />
+              </div>
+              <div className="pt-2">
+                <CategoryFilter />
+                <RecentListings />
+                <ListingSaleGrid />
+              </div>
+              </>
+              
+            } />
+            <Route path="/ListingPage" element={<ListingPage />} />
+            <Route path="/rentlist" element={<RentList />} />
+            <Route path="/signin" element={<AuthForm />} />
+            <Route path="/register" element={<AuthForm />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/explore" element={<ExplorePage />} />
+          </Routes>
+          <FooterSection />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
