f96d528801629613cfa7f8f62fd686e36b5aa89b
EloM28 <mugisheloge@gmail.com>
2025-07-17 10:49:21 +0200

    Added tabsconfig

---
+++ src/components/dashboardTabsConfig.js
@@ -0,0 +1,8 @@
+export const dashboardTabsMenu = [
+  { label: "Dashboard", value: "Dashboard" },
+  { label: "My Listings", value: "My Listings" },
+  { label: "Promotions", value: "Promotions" },
+  { label: "Bookmarks", value: "Bookmarks" },
+  { label: "Account details", value: "Account details" },
+  { label: "Logout", value: "Logout", danger: true },
+]; 
