f7b9327775656eb3be522223a3ce091effc1c18e
EloM28 <mugisheloge@gmail.com>
2025-07-17 16:13:44 +0200

    Changed the DashBoardTabs

--- src/components/DashboardTabs.jsx
+++ src/components/DashboardTabs.jsx
@@ -19,7 +19,7 @@ const DashboardTabs = ({ tabs, activeTab, onTabChange }) => {
             if (tab.danger) return handleLogout();
             onTabChange(tab.value);
           }}
-          className={`font-bold text-sm md:text-base px-4 md:px-8 py-3 md:py-4 border-b-2 transition-colors duration-150 
+          className={`font-bold text-sm md:text-base px-4 md:px-8 py-3 md:py-4 border-b-2 transition-colors duration-150 cursor-pointer
             ${ activeTab === tab.value ? "border-red-500 text-gray-900" : "border-transparent text-gray-700 hover:text-red-500"} flex-1 min-w-[100px] max-w-[150px] ${tab.danger ? 'text-red-500' : ''}`}
         >
           {tab.label}
