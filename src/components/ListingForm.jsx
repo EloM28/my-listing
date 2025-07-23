e466be8bc79d68fbf75a3bf29a13ae5d123f766e
EloM28 <mugisheloge@gmail.com>
2025-07-23 17:59:41 +0200

    Changed the ListingForm

--- src/components/ListingForm.jsx
+++ src/components/ListingForm.jsx
@@ -151,11 +151,11 @@ const ListingForm = () => {
                     <label className="font-medium mb-2">Region <span className="text-gray-500">(optional)</span>
                       <select className="block w-full bg-gray-50 border border-gray-200 rounded px-4 py-2 mt-2 text-base text-gray-700 focus:outline-none focus:ring-2 focus:ring-red-200 appearance-none" defaultValue="">
                         <option value="" disabled>Select an option</option>
-                        <option value="paris">Paris</option>
-                        <option value="lyon">Lyon</option>
-                        <option value="marseille">Marseille</option>
-                        <option value="bordeaux">Bordeaux</option>
-                        <option value="autre">Autre</option>
+                        <option value="paris">Bujumbura</option>
+                        <option value="lyon">Gitega</option>
+                        <option value="marseille">Buhumuza</option>
+                        <option value="bordeaux">Burunga</option>
+                        <option value="autre">Butanyerera</option>
                       </select>
                     </label>
                     <label className="font-medium mt-4">Location
